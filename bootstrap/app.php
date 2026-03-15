3880adb3fd4cf6e076ebb31bda6816c727e179ae
khaeru99 <khaerul.r.23@gmail.com>
2026-03-15 10:41:18 +0700

    update upload image

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,17 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
+$app->useStoragePath('/tmp/storage');
+
+// Dan pastikan folder storage-nya siap (tambahkan ini juga)
+if (!is_dir('/tmp/storage/app/public')) {
+    mkdir('/tmp/storage/app/public', 0755, true);
+    mkdir('/tmp/storage/framework/views', 0755, true);
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',

43d4c8ea24f8e8a1de73080ff85110181dfa5103
khaeru99 <khaerul.r.23@gmail.com>
2026-03-16 16:25:39 +0700

    update model testimoni

--- app/Models/Testimony.php
+++ app/Models/Testimony.php
@@ -8,4 +8,19 @@ class Testimony extends Model
 {
     //
    protected $fillable = ['name', 'photo', 'job', 'rating', 'content', 'is_published'];
+
+   protected function photo(): Attribute
+{
+    return Attribute::make(
+        get: function ($value) {
+            
+            if (!$value) return null;
+            if (str_starts_with($value, 'http')) {
+                return $value;
+            }
+
+            return Storage::disk('cloudinary')->url($value);
+        },
+    );
+}
 }
